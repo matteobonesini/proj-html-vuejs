af647a592c30a9870fe08d92258f9c63bc3bd7c4
matteobonesini <matteo.bonesini@gmail.com>
2023-07-25 10:25:39 +0200

    add: social section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -255,6 +255,42 @@ export default {
             </div>
         </section>
 
+        <section class="social">
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-1.jpg" alt="social-1">
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex flex-column justify-content-center align-items-center h-100">
+                            <h6 class="subtitle">Made for sharing</h6>
+                            <h3 class="title">Let's stay in touch</h3>
+                            <button class="secondary">
+                                <i class="fa-brands fa-instagram me-2"></i>
+                                Follow Us
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-2.jpg" alt="social-2">
+                    </div>
+
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-3.jpg" alt="social-3">
+                    </div>
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-4.jpg" alt="social-4">
+                    </div>
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-5.jpg" alt="social-5">
+                    </div>
+                    <div class="col-3">
+                        <img class="w-100" src="../assets/img/social-6.jpg" alt="social-6">
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <section class="call-to-action">
             <div class="call-container d-flex flex-column justify-content-center align-items-start">
                 <h6 class="subtitle text-light">Try our season products</h6>
