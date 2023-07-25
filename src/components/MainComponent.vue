b459fcb02011e8284f19013f651ada21483a1965
matteobonesini <matteo.bonesini@gmail.com>
2023-07-25 09:15:27 +0200

    add: bakery-process section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -144,6 +144,72 @@ export default {
             </div>
         </section>
 
+        <section class="bakery-process">
+            <div class="container">
+                <div class="tell-us">
+                    <div class="overlay"></div>
+                    <h2 class="title text-light fs-1">Tell us what you have in mind</h2>
+                    <div class="order  text-light">01</div>
+                    <div class="description">
+                        <h6 class="subtitle text-uppercase  text-light">Manual ingredients selection</h6>
+                        <p class="text-light">Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl varius. Ut
+                            eu
+                            laoreeet ex.
+                            Aliquam
+                            erat volutpat. Nullam quis sagittis nibh. Morbi consectetur ultricies ante ac congue. Donec
+                            lorem
+                            erat,
+                            finibus in velit placerat, rutrum augue.</p>
+                    </div>
+                </div>
+
+                <div class="process-center d-flex">
+                    <div class="we-start">
+                        <div class="overlay"></div>
+                        <h2 class="title text-light fs-1">We Start Baking</h2>
+                        <div class="order  text-light">02</div>
+                        <div class="description">
+                            <h6 class="subtitle text-uppercase  text-light">Sweet & delicius</h6>
+                            <p class="text-light">Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl
+                                varius. Ut
+                                eu
+                                laoreeet ex.
+                                Aliquam
+                                erat volutpat. Nullam quis sagittis nibh. Morbi consectetur ultricies ante ac congue. Donec
+                                lorem
+                                erat,
+                                finibus in velit placerat, rutrum augue.</p>
+                        </div>
+                    </div>
+
+                    <div class="quote d-flex flex-column justify-content-center">
+                        <h6 class="subtitle text-uppercase">Dont just take our word for it</h6>
+                        <h2 class="title">"Finally found an alternative to the mass produced products. Somethings that
+                            incorporates real organic ingredients, nutrient dense wellness while promoting sustainability
+                            and activity."</h2>
+                        <p>Rachel Cooper, Founder</p>
+                    </div>
+                </div>
+
+                <div class="delivered">
+                    <div class="overlay"></div>
+                    <h2 class="title text-light fs-1">Delivered to your home</h2>
+                    <div class="order  text-light">03</div>
+                    <div class="description">
+                        <h6 class="subtitle text-uppercase  text-light">Baked to order $ gift wrapped</h6>
+                        <p class="text-light">Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl varius. Ut
+                            eu
+                            laoreeet ex.
+                            Aliquam
+                            erat volutpat. Nullam quis sagittis nibh. Morbi consectetur ultricies ante ac congue. Donec
+                            lorem
+                            erat,
+                            finibus in velit placerat, rutrum augue.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 </template>
 
